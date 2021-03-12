9563f2ee3ea641f84b55c81d260420ec304870ac
linlei <512102250@qq.com>
2021-03-12 13:18:48 +0800

    ‘09’

--- Week 09/server.js
+++ Week 09/server.js
@@ -8,8 +8,30 @@ http.createServer((request,response) => {
     }).on('end', () => {
         body = body.join("");
         console.log('body',body);
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.end(' Hello World\n')
+        response.setHeader('Content-Type','text/html');
+        response.setHeader('X-Foo','bar');
+        response.writeHead(200,{'Content-Type':'text/plain'});
+
+        response.end(
+        `<html maaa=a>
+        <head>
+            <style>
+        body div #myid{
+            width:100px;
+            background-color:#ff5000;
+        }
+        body div img{
+            width:300px;
+            background-color:#ff1111;
+        }
+            </style>
+        </head>
+        <body>
+            <div>
+                <img id='myid'/>
+                <img />
+            </div>    
+        </html> `);
     })
 }).listen(8080)
 
