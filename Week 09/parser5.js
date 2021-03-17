4464fc3615656376d4f63c18d6734e87ef8abfab
512102250@qq.com <512102250@qq.com>
2021-03-17 21:51:26 +0800

    09

--- Week 09/parser5.js
+++ Week 09/parser5.js
@@ -1,6 +1,5 @@
 
 const css = require('css');
-const { EMLINK } = require('node:constants');
 const EOF = Symbol('EOF');
 let currentToken = null;
 let currentAttribute =  null;
@@ -9,13 +8,112 @@ let currentTextNode = null;
 let rules = [];
 function addCSSRules(text) {
     var ast = css.parse(text);
-    console.log(JSON.stringify(ast,null,"   "));
+    // console.log(JSON.stringify(ast,null,"   "));
     rules.push(...ast.stylesheet.rules)
 }
 function computeCss (element) {
-    console.log(rules);
-    console.log('compute css for element',element)
+    // console.log(rules);
+    // console.log('compute css for element',element)
+    var elements = stack.slice().reverse();
+
+    if(!element.computedStyle){
+        element.computedStyle = {}
+    }
+
+    for(let rule of rules){
+        var selectorParts = rule.selectors[0].split(" ").reverse();
+        if(!match(element,selectorParts[0])){
+            continue;
+        }
+
+        let matched = false;
+
+        var j = 1;
+        for(let i= 0; i<elements.length;i++){
+            if(match(elements[i],selectorParts[j])){
+                j++
+            }
+        }
+
+        if(j >= selectorParts.length){
+            matched = true;
+        }
+
+        if(matched){
+            const sp = specificity(rule.selectors[0]);
+           var computedStyle = element.computedStyle;
+             for (let del of rule.declarations) {
+                if (!computedStyle[del.property]) {
+                    computedStyle[del.property] = {};
+                }
+
+                if (!computedStyle[del.property].specificity) {
+                    computedStyle[del.property].value = del.value;
+                    computedStyle[del.property].specificity = sp;
+                } else if (compare(computedStyle[del.property].specificity, sp) < 0) {
+                    computedStyle[del.property].value = del.value;
+                    computedStyle[del.property].specificity = sp;
+                }
+            }
+           console.log(element.computedStyle)
+        }
+    }
 }
+function match(element, selector) {
+    if (!selector || !element.attributes) {
+        return false;
+    }
+
+    if (selector.charAt(0) === "#") {
+        const attr = element.attributes.filter(attr => attr.name === "id")[0];
+        if (attr && attr.value === selector.replace("#", "")) {
+            return true;
+        }
+    } else if (selector.charAt(0) === ".") {
+        const attr = element.attributes.filter(attr => attr.name === "class")[0];
+        if (attr && attr.value === selector.replace(".", "")) {
+            return true;
+        }
+    } else {
+        if (element.tagName === selector) {
+            return true;
+        }
+    }
+
+    return false;
+}
+function specificity(selector) {
+    let p = [0, 0, 0, 0];
+    const selectorParts = selector.split(" ");
+    for (let part of selectorParts) {
+        if (part.charAt(0) === "#") {
+            p[1] += 1;
+        } else if (part.charAt(0) === ".") {
+            p[2] += 1;
+        } else {
+            p[3] += 1;
+        }
+    }
+
+    return p;
+}
+
+function compare(sp1, sp2) {
+    if (sp1[0] - sp2[0]) {
+        return sp1[0] - sp2[0];
+    }
+
+    if (sp1[1] - sp2[1]) {
+        return sp1[1] - sp2[1];
+    }
+
+    if (sp1[2] - sp2[2]) {
+        return sp1[2] - sp2[2];
+    }
+
+    return sp1[3] - sp2[3];
+}
+
 function emit(token){
   
     let top = stack[stack.length -1 ];
